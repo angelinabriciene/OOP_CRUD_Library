66137ddb426f949b04187089bedd94a78405ca5a
angelinabriciene <aburbaite865@gmail.com>
2024-05-27 16:31:27 +0300

    3 iš 5 CRUD

--- src/Author.java
+++ src/Author.java
@@ -1,9 +1,12 @@
-public class Author {
-    private int id;
-    private String name;
-    private String surename;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public int getId() {
+public class Author {
+    private long id;
+    private String name;
+    private String surname;
+
+    public long getId() {
         return id;
     }
 
@@ -19,19 +22,74 @@ public class Author {
         this.name = name;
     }
 
-    public String getSurename() {
-        return surename;
+    public String getSurname() {
+        return surname;
     }
 
-    public void setSurename(String surename) {
-        this.surename = surename;
+    public void setSurname(String surename) {
+        this.surname = surename;
     }
+
     public Author() {
     }
 
     public Author(int id, String name, String surename) {
         this.id = id;
         this.name = name;
-        this.surename = surename;
+        this.surname = surename;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surename='" + surname + '\'' +
+                '}';
+    }
+
+    public static void addAuthor(Scanner sc, ArrayList<Author> libraryAuthors) {
+        Author aut = new Author();
+//        int authorCount = 0;
+        System.out.println("Įveskite autoriaus vardą");
+        aut.setName(sc.nextLine());
+        System.out.println("Įveskite autoriaus paradę");
+        aut.setSurname(sc.nextLine());
+//        authorCount++;
+//        aut.setId(authorCount);
+        System.out.println("Įveskite ID numerį");
+        aut.setId(sc.nextInt());
+        System.out.println("Autorius užregistruotas.");
+        libraryAuthors.add(aut);
+    }
+
+    public static void listAuthors(ArrayList<Author> libraryAuthors) {
+        System.out.println("Autorių sąrašas: " + libraryAuthors);
+    }
+
+    public static void editAuthor(Scanner sc, ArrayList<Author> libraryAuthors) {
+        System.out.println("Įveskite autoriaus ID, kurį norite redaguoti:");
+        long oldID = sc.nextLong();
+        sc.nextLine();
+        for (int i = 0; i < libraryAuthors.size(); i++) {
+            Author author = libraryAuthors.get(i);
+            if (author.getId() == oldID) {
+                System.out.println("Įveskite naują autoriaus vardą:");
+                String newName = sc.nextLine();
+                System.out.println("Įveskite naują autoriaus pavardę:");
+                String newSurname = sc.nextLine();
+                System.out.println("Įveskite naują autoriaus ID:");
+                long newID = sc.nextLong();
+                sc.nextLine();
+                author.setName(newName);
+                author.setSurname(newSurname);
+                if (newID != 0) {
+                    author.setId((int) newID);
+                }
+                System.out.println("Autorius sėkmingai atnaujintas!");
+                return;
+            }
+        }
+        System.out.println("Autorius nerastas.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Sveiki atvykę į rekomendacijų biblioteką. Pasirinkite vieną iš funkcijų:");
+        System.out.println("1. Įvesti naują autorių");
+        System.out.println("2. Pamatyti visą autorių sąrašą");
+        System.out.println("3. Redaguoti pasirinktą autorių");
+        System.out.println("4. Ištrinti autorių iš sąrašo");
+        System.out.println("5. Išeiti iš programos");
+        System.out.println("---------------");
         ArrayList<Author> libraryAuthors = new ArrayList<>();
-        Scanner sc = new Scanner();
+        Author a = new Author();
+        a.setId(5555);
+        a.setName("Petras");
+        a.setSurname("Petraitis");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int input = sc.nextInt();
+            sc.nextLine();
+            switch (input) {
+                case 1:
+                    Author.addAuthor(sc,libraryAuthors);
+                    break;
+                case 2:
+                    Author.listAuthors(libraryAuthors);
+                    break;
+                case  3:
+                    Author.editAuthor(sc, libraryAuthors);
+                    break;
+                case 4:
+
+
+
+
+            }
+        }
     }
 }
